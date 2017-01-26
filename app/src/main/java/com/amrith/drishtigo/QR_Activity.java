bbd08a66800d715b79b7ae6449f0a034bb7b8290
Athul <athulkrishnaa@gmail.com>
2017-01-26 22:45:24 +0530

    pokesprites file n pokearrays with rarity in java added

--- app/src/main/java/com/amrith/drishtigo/QR_Activity.java
+++ app/src/main/java/com/amrith/drishtigo/QR_Activity.java
@@ -1,6 +1,6 @@
 package com.amrith.drishtigo;
 
-import android.content.pm.PackageManager;
+import android.content.pm.PackageManager ;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,14 +17,26 @@ import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.IOException;
 
-public class QR_Activity extends AppCompatActivity {
+public class QR_Activity extends AppCompatActivity{
 
     TextView barcodeInfo;
     SurfaceView cameraView;
     BarcodeDetector barcodeDetector;
     CameraSource cameraSource;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+ String[] poknames	={'Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise', 'Caterpie', 'Metapod', 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', 'Pidgeot', 'Rattata', 'Raticate', 'Spearow', 'Fearow', 'Ekans', 'Arbok', 'Pikachu', 'Raichu', 'Sandshrew', 'Sandslash', 'NidoranF', 'Nidorina', 'Nidoqueen', 'NidoranM', 'Nidorino', 'Nidoking', 'Clefairy', 'Clefable', 'Vulpix', 'Ninetales', 'Jigglypuff', 'Wigglytuff', 'Zubat', 'Golbat', 'Oddish', 'Gloom', 'Vileplume', 'Paras', 'Parasect', 'Venonat', 'Venomoth', 'Diglett', 'Dugtrio', 'Meowth', 'Persian', 'Psyduck', 'Golduck', 'Mankey', 'Primeape', 'Growlithe', 'Arcanine', 'Poliwag', 'Poliwhirl', 'Poliwrath', 'Abra', 'Kadabra', 'Alakazam', 'Machop', 'Machoke', 'Machamp', 'Bellsprout', 'Weepinbell', 'Victreebel', 'Tentacool', 'Tentacruel', 'Geodude', 'Graveler', 'Golem', 'Ponyta', 'Rapidash', 'Slowpoke', 'Slowbro', 'Magnemite', 'Magneton', "Farfetch'd", 'Doduo', 'Dodrio', 'Seel', 'Dewgong', 'Grimer', 'Muk', 'Shellder', 'Cloyster', 'Gastly', 'Haunter', 'Gengar', 'Onix', 'Drowzee', 'Hypno', 'Krabby', 'Kingler', 'Voltorb', 'Electrode', 'Exeggcute', 'Exeggutor', 'Cubone', 'Marowak', 'Hitmonlee', 'Hitmonchan', 'Lickitung', 'Koffing', 'Weezing', 'Rhyhorn', 'Rhydon', 'Chansey', 'Tangela', 'Kangaskhan', 'Horsea', 'Seadra', 'Goldeen', 'Seaking', 'Staryu', 'Starmie', 'Mr. Mime', 'Scyther', 'Jynx', 'Electabuzz', 'Magmar', 'Pinsir', 'Tauros', 'Magikarp', 'Gyarados', 'Lapras', 'Ditto', 'Eevee', 'Vaporeon', 'Jolteon', 'Flareon', 'Porygon', 'Omanyte', 'Omastar', 'Kabuto', 'Kabutops', 'Aerodactyl', 'Snorlax', 'Articuno', 'Zapdos', 'Moltres', 'Dratini', 'Dragonair', 'Dragonite', 'Mewtwo', 'Mew'}
+
+int[] very_common	={9, 12, 15, 18, 40, 42, 49, 71, 73, 78, 80, 97, 108, 119, 128}
+int[] common		={10, 13, 16, 19, 20, 22, 26, 43, 45, 47, 55, 59, 65, 68, 72, 83, 87, 89, 91, 99, 107, 113, 117, 132}
+int[] uncommon		={0, 3, 6, 11, 14, 17, 21, 23, 27, 28, 31, 38, 41, 44, 46, 48, 50, 53, 60, 62, 66, 69, 74, 76, 79, 81, 84, 85, 88, 90, 92, 94, 98, 100, 101, 103, 105, 109, 112, 115, 120, 123, 124, 125, 126, 127, 131}
+int[] more_uncommon	={1, 4, 7, 24, 29, 32, 34, 36, 39, 51, 54, 56, 57, 61, 63, 67, 70, 77, 82, 86, 95, 102, 104, 106, 110, 116, 118, 122, 133, 134, 135, 142, 146}
+int[] rare		={2, 5, 8, 25, 30, 33, 35, 37, 52, 64, 75, 93, 96, 111, 114, 121, 129, 137, 139, 141, 147}
+int[] super_rare	={130, 136, 138, 140, 148}
+int[] legendary		={58, 143, 144, 145, 149, 150}
+
+
+protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr_);
         cameraView = (SurfaceView) findViewById(R.id.camera_view);
